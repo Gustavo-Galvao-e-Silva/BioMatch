f578d61e93ff1a0a243d6687405d86bb16adaa01
Caio Carraro Bahlis <caiobahlis@eduroam-226-126-206.laptops.usf.edu>
2026-03-29 01:54:57 -0400

    Add Support for chat between patient and researcher

--- src/frontend/src/app/routes.ts
+++ src/frontend/src/app/routes.ts
@@ -13,6 +13,7 @@ import DoctorDashboard from "./pages/doctor/Dashboard";
 
 import ResearcherDashboard from "./pages/researcher/Dashboard";
 import ClaimResearch from "./pages/researcher/ClaimResearch";
+import PatientMessagesPage from "./pages/PatientMessagesPage";
 
 export const router = createBrowserRouter([
   { path: "/login/*", Component: LoginPage, },
@@ -23,6 +24,7 @@ export const router = createBrowserRouter([
   { path: "/auth-test", Component: AuthTestPage, },
   
   { path: "/patient", Component: PatientDashboard, },
+  { path: "/patient/messages", Component: PatientMessagesPage, },
 
   { path: "/doctor", Component: DoctorDashboard, },
 
